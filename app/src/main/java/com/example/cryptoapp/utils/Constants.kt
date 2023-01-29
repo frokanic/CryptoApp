7f087a32fe70aaee87ecf50d6b6401c1738d085e
frokanic <f.rokanis@gmail.com>
2023-01-29 17:51:07 +0200

    Initial commit

---
+++ app/src/main/java/com/example/cryptoapp/utils/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.stockmarketapp.utils
+
+object Constants {
+
+    const val BASE_URL_ALPHAVANTAGE = "https://alphavantage.co"
+    const val API_KEY_ALPHAVANTAGE = "MID44R1E8985VTXX"
+
+    const val BASE_URL_POLYGON = "https://api.polygon.io"
+    const val API_KEY_POLYGON = "xLFHd1pbfIiucUqYap4n0gQLdZX9BMff"
+
+}
